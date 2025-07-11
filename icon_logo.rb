f34a2edaebcaded3960a2f336b67bc4c96b9bc55
漫客信息 <1210485317@qq.com>
2025-07-11 23:43:22 +0800

    build(icon_logo): 更新 macOS 下载链接和文件类型
    - 将下载链接从 .tar.gz 文件改为 .tar.gz 文件
    - 修正

--- icon_logo.rb
+++ icon_logo.rb
@@ -1,7 +1,7 @@
 class IconLogo < Formula
   desc "多尺寸 logo/icon 生成工具，支持圆角裁剪"
   homepage "https://github.com/zk3151463/icon_logo"
-  url "https://github.com/zk3151463/icon_logo/releases/download/v1.0.0/icon_logo_macos"
+  url "https://github.com/zk3151463/icon_logo/releases/download/v1.0.0/icon_logo-macos.tar.gz"
   version "1.0.0"
   sha256 "1ebc62bae560b37614daade460beed14242ef5b6c7d2252abd858df3ad4c7a3e"
 
