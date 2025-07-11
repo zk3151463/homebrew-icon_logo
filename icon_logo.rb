443418b4a666544c5d0f04da353806979f1f8aec
漫客信息 <1210485317@qq.com>
2025-07-12 00:40:45 +0800

    icon_logo: update formula to version 1.0.3

--- icon_logo.rb
+++ icon_logo.rb
@@ -1,15 +1,17 @@
 class IconLogo < Formula
-  desc "多尺寸 logo/icon 生成工具，支持圆角裁剪"
+  desc "图标生成工具"
   homepage "https://github.com/zk3151463/icon_logo"
-  url "https://github.com/zk3151463/icon_logo/releases/download/v1.0.0/icon_logo-macos.tar.gz"
-  version "1.0.0"
-  sha256 "1ebc62bae560b37614daade460beed14242ef5b6c7d2252abd858df3ad4c7a3e"
+  url "https://github.com/zk3151463/icon_logo/archive/refs/tags/v1.0.3.tar.gz"
+  sha256 "f20828adc28a7a4df3a21ab197ca81b659c0e1fee0b93a74914d3682a3b979f9"
+  license "MIT"
+
+  depends_on "go" => :build
 
   def install
-    bin.install "icon_logo"
+    system "go", "build", *std_go_args
   end
 
   test do
-    system "icon_logo", "--help"
+    assert_match "Usage", shell_output("\#{bin}/icon_logo --help")
   end
 end
