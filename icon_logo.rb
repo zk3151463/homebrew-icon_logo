545379cc910797795d45e5db17289559eed0bb3c
漫客信息 <1210485317@qq.com>
2025-07-12 01:14:58 +0800

    icon_logo: update formula to version 1.0.8

--- icon_logo.rb
+++ icon_logo.rb
@@ -1,8 +1,8 @@
 class IconLogo < Formula
   desc "图标生成工具"
   homepage "https://github.com/zk3151463/icon_logo"
-  url "https://github.com/zk3151463/icon_logo/archive/refs/tags/v1.0.7.tar.gz"
-  sha256 "fb5ad4111e34ffad6bff328994724c6195d47ae9305bfe6304bd2d263961d29b"
+  url "https://github.com/zk3151463/icon_logo/archive/refs/tags/v1.0.8.tar.gz"
+  sha256 "76e8b9b1e2bedb4018151335fa94f6e9984102b73f74ee8f97438ca92ae65cf1"
   license "MIT"
 
   depends_on "go" => :build
